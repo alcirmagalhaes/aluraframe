788d113cccf5deb562e99508356a4142a1788c95
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-19 11:08:10 -0300

    Aula02-8

--- client/js/models/Negociacao.js
+++ client/js/models/Negociacao.js
@@ -1,6 +1,6 @@
 class Negociacao {
     constructor(data, quantidade, valor){
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
 
@@ -13,7 +13,7 @@ class Negociacao {
     }
 
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
