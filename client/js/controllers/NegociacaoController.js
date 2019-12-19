7f0dd5e7662a0ac5db02539b9ef52791f782f335
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-19 14:10:14 -0300

    Aula02-2

--- client/js/controllers/NegociacaoController.js
+++ client/js/controllers/NegociacaoController.js
@@ -2,6 +2,13 @@ class NegociacaoController {
 
     adiciona(event) {
           event.preventDefault();
-          alert('Chamei ação no controller');
+          let $ = document.querySelector.bind(document);
+          let inputData = $('#data');
+          let inputQuantidade = $('#quantidade');
+          let inputValor = $('#valor');
+
+          console.log(inputData.value);
+          console.log(inputQuantidade.value);
+          console.log(inputValor.value);
     }
   }
