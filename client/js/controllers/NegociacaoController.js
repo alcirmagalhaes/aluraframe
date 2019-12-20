d3bfd3d80b137147daab1ead3e6b5b8919f3038f
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-20 09:42:20 -0300

    Aula03-10

--- client/js/controllers/NegociacaoController.js
+++ client/js/controllers/NegociacaoController.js
@@ -8,19 +8,16 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        //diminuindo a verbosidade no map
         let data = new Date(
             ...this._inputData.value
                    .split('-')
                    .map((item, indice) => item - indice % 2)
         );
-        
         let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
             this._inputValor.value
         )
         console.log(negociacao);
-
     }
   }
