d7b75090a5817f69a093bad95f77a6f705152c9d
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-20 15:18:00 -0300

    Aula04-14

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -4,7 +4,7 @@ class DateHelper {
         throw new Error('Esta classe não pode ser instânciada!');
     }
     static textoParaData(texto){
-        if (!/\d{4}-\d{2}-\d{2}/.test(texto))
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
             throw new Error('Deve estar no formato  yyyy-mm-dd')
         return new Date(...texto.split('-').map((item,indice) => item - indice % 2));
     }
