88cbe92ea97b44eba473196e329bdda70bf045e6
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-20 14:30:29 -0300

    Aula040-5

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,19 +4,28 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
-        let data = DateHelper.textoParaData(this._inputData.value);
-        let negociacao = new Negociacao(
-            data,
+        let negociacao = this._criaNegociacao();
+        this._listaNegociacoes.adiciona(negociacao);
+        this._limpaFormulario();    
+        console.log(this._listaNegociacoes.negociacoes); 
+    }
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0;
+        this._inputData.focus();
+        
+    }
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         )
-        console.log(negociacao);
-        //fazer a data ser exibina no formato dd/mm/yyyy
-        let diaMesAno = DateHelper.dataParatexto(data)
-        console.log(diaMesAno); 
     }
   }
