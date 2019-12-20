3e0520cf74a939b220a90109dd908824fe634357
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-20 09:28:34 -0300

    Aula03-8

--- client/js/controllers/NegociacaoController.js
+++ client/js/controllers/NegociacaoController.js
@@ -8,11 +8,36 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        console.log(typeof(this._inputData.value));
         
-        console.log(this._inputData.value);
-        //a data é recebida no formato '2019-12-19' e a transformamos em ['2019','12','19'] para o construtor Date
-        let data = new Date(this._inputData.value.split('-'));  
-        console.log(data)
+        //com o split funciona perfeitamente (pois ele transfroma a data em um 
+        //array de 3 elementos ['YYYY', 'MM', 'DD'], e o construtor Date 
+        //trabalha perfeitamente com este array)
+        let data1 = new Date(
+            this._inputData.value.split('-')
+        );
+        console.log(data1);
+        console.log(data1)
+        console.log('----');
+        //Utilizando programação funciomal
+        let data = new Date(
+            //O split cria o array ['yyyy', 'mm','dd']
+            //O map varre o array retornado pelo split e decrementa o mês (indice = 1) em uma unidade e retorna um novo array
+            //O '...' é chamado de spread operator e ele desmembra o array em YYYY MM DD
+            ...this._inputData.value
+                .split('-')
+                .map(function(item, indice){
+                    return item - indice % 2; // como o vetor só tem três posiçoes isto dá certo para decrementar apenas o mês
+                })
+        );
+        console.log(data);
+        console.log(data);
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+        console.log(negociacao);
+
     }
   }
