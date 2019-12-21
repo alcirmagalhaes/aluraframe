1204c38a0d21813ccc87804bf8d3e1823a8e5d02
alcir <alcirmagalhaes@gmail.com>
2019-12-21 11:56:59 -0300

    Aula05-1

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,27 +1,35 @@
 class NegociacaoController {
     constructor () {
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update();
     }
 
     adiciona(event) {
+
         event.preventDefault();
         let negociacao = this._criaNegociacao();
         this._listaNegociacoes.adiciona(negociacao);
         this._limpaFormulario();    
         console.log(this._listaNegociacoes.negociacoes); 
     }
+ 
     _limpaFormulario(){
+ 
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0;
         this._inputData.focus();
         
     }
+ 
     _criaNegociacao(){
+ 
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
