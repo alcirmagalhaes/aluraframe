77c7d811ad730d9a2f703d1ff95ccaff86df8c85
alcir <alcirmagalhaes@gmail.com>
2019-12-22 22:07:12 -0300

    Aula05-5

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -33,14 +33,9 @@ class NegociacoesView {
                 
                 <tfoot>
                     <td colspan="3"> </td>
-                    <td> ${ 
-                        //IIFE - Immediately-invoked function expression (Funcão Imediata)
-                        (function() {
-                            let total = 0;
-                            model.negociacoes.forEach(n => total += n.volume);
-                            return total;
-                        })()
-                    } </td>
+                    <td> 
+                        ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                    </td>
                 </tfoot>
             </table> 
         `;
