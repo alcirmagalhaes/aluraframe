5ee81de38871b59d648d8ca0f4a9335740cdca42
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-23 12:02:25 -0300

    Aula06-2

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,10 +5,14 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
         this._listaNegociacoes = new ListaNegociacoes();
-        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
@@ -17,6 +21,10 @@ class NegociacaoController {
         const negociacao = this._criaNegociacao();
         this._listaNegociacoes.adiciona(negociacao);
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso!'
+        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();    
         //console.log(this._listaNegociacoes.negociacoes); 
     }
