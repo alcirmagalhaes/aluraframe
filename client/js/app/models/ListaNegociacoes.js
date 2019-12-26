6bbe4c314d14376871d45461b847ea3a5aa2d4bb
alcir <alcirmagalhaes@gmail.com>
2019-12-26 20:23:11 -0300

    Aula2-01-4

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,14 @@
 class ListaNegociacoes{
-    constructor(contexto, armadilha){
+    constructor(armadilha){
+      
         this._negociacoes = [];
         this._armadilha = armadilha;   
-        this._contexto = contexto;
     }
 
     adiciona(negociacao) {
+       
         this._negociacoes.push(negociacao);
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto,[this]);
+        this._armadilha(this);
     }
 
     get negociacoes(){
@@ -20,7 +20,6 @@ class ListaNegociacoes{
     esvazia() {
 
         this._negociacoes = [];
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto,[this]);
+        this._armadilha(this);
     }
 }
