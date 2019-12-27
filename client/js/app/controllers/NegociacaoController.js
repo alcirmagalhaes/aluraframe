e3dd0e2e94f3b42f1f39ef24264f59b6b2a435f7
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-27 14:47:21 -0300

    Aula2-03-3

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,18 +6,17 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = ProxyFactory.create (
-            new ListaNegociacoes(),
-            ['adiciona', 'esvazia'], model =>
-                this._negociacoesView.update(model));
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        //this._negociacoesView.update(this._listaNegociacoes);
+        this._listaNegociacoes = new Bind (
+            new ListaNegociacoes(),
+            this._negociacoesView,
+            ['adiciona', 'esvazia']);
 
-        this._mensagem = ProxyFactory.create(
-            new Mensagem(), ['texto'], model => 
-                this._mensagemView.update(model));
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            this._mensagemView,
+            ['texto']);
     }
 
     adiciona(event) {
@@ -51,7 +50,7 @@ class NegociacaoController {
 
     apaga() {
        
-        this._listaNegociacoes.esvazia();
+       this._listaNegociacoes.esvazia();
        this._mensagem.texto = 'Lista de negociações apagada!';
        
     }
