908669e3477ff76bfc80166f7be5cdc928cef9c5
Jose Alcir de Medeiros Magalhaes <jose-alcir.magalhaes@serpro.gov.br>
2019-12-27 13:59:32 -0300

    aula2-03-2

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -3,7 +3,7 @@ class ProxyFactory {
 
         return new Proxy(objeto, {
             get(target, prop, receiver) {
-                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
                     return function() {
 
                         console.log(`a propriedade "${prop}" foi interceptada`);
@@ -12,7 +12,18 @@ class ProxyFactory {
                     }
                 }
                 return Reflect.get(target, prop, receiver);       
+            },
+            set (target, prop, value, receiver) {
+                if (props.includes(prop)) {
+                    acao(target);
+                }
+                return Reflect.set(target, prop, value, receiver);
+                
             }
         })
+    }
+    static _ehFuncao(func) {
+
+        return typeof(func) == typeof(Function)
     }
 }
