b979d03b9c00d309dceab1b599fc7e4be5a76e97
alcir <alcirmagalhaes@gmail.com>
2020-01-06 20:36:30 -0300

    Aula3-02-2

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,46 @@
+var stores = ['negociacoes'];
+var version = 4;
+var dbName = 'aluraframe';
+
+class ConnectionFactory {
+    constructor(){
+
+        throw new Error("Não é possível criar instâncias de ConnectionFactory");
+    }
+
+    static getConnection() {
+
+        return new Promise( (resolve, reject) => {
+
+            let openRequest = window.indexedDB.open(dbName,version);
+
+            openRequest.onupgradeneeded = e => {
+
+                ConnectionFactory._createStores(e.target.result);
+            };
+            
+            openRequest.onsuccess = e => {    
+
+                resolve(e.target.result);
+            };
+            
+            openRequest.onerror = e => {    
+
+                console.log(e.target.error);
+            
+                reject(e.target.error.name);
+            };
+        });
+    };
+    
+    static _createStores(connection) {
+        
+        stores.forEach(store => {
+            if (connection.objectStoreNames.contains(store)) {
+                
+                connection.deleteObjectStore(store);
+            };    
+            connection.createObjectStore(store, { autoIncrement: true });
+        });
+    }
+}
