a09dd3d5edc149b85ee21838d1917309146e7f85
alcir <alcirmagalhaes@gmail.com>
2020-01-07 18:51:34 -0300

    Aula3-02-5

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,10 +1,11 @@
 var ConnectionFactory = (function() {
 
-    var stores = ['negociacoes'];
-    var version = 4;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
     
     var connection = null;
+    var close = null;
     
     return class ConnectionFactory {
         constructor(){
@@ -25,7 +26,15 @@ var ConnectionFactory = (function() {
                 
                 openRequest.onsuccess = e => {    
     
-                    if (!connection) connection = e.target.result;
+                    if (!connection) {
+                        
+                        connection = e.target.result;
+                        
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error("Você não pode fechar diretamente a conexão!")
+                        }
+                    }
                     resolve(connection);
                 };
                 
@@ -48,6 +57,15 @@ var ConnectionFactory = (function() {
                 connection.createObjectStore(store, { autoIncrement: true });
             });
         };
+
+        static closeConnection() {
+
+            if (connection) {
+
+                close();
+                connection = null;
+            }
+        }
     };
 })()
 
